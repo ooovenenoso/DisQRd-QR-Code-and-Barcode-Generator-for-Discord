d986d810d9171f262bfe7270a4af6f3113747272
ooopoison <120500656+ooopoison@users.noreply.github.com>
2023-01-04 20:37:54 -0400

    Fix issue w access to message content in channel
    Fix issue with access to message content in channel
    
    Added the line `intents.message_content = True` to enable the message content intent and allow the bot to access the content of messages in the channel.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
 intents.typing = True
+intents.message_content = True  # Add this line to enable the message contents intent
 
 # Create Discord bot
 bot = commands.Bot(command_prefix="/", intents=intents)
@@ -18,31 +19,31 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 # QR code generation command
 @bot.command()
 async def qr(ctx, url: str):
-  """Generates a QR code for the given URL and sends it in the current channel.
-  
-  Usage: /qr <url>
-  """
-  
-  # Create QR code object
-  qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-  )
-  qr.add_data(url)
-  qr.make(fit=True)
-  
-  # Generate QR code image
-  img = qr.make_image(fill_color="black", back_color="white")
-  
-  # Save QR code image to file
-  with open("qr.png", "wb") as f:
-    img.save(f)
-  
-  # Send QR code image in current channel
-  await ctx.send("Aquí está tu código QR:", file=discord.File("qr.png"))
+    """Generates a QR code for the given URL and sends it in the current channel.
+    
+    Usage: /qr <url>
+    """
+
+    # Create QR code object
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    # Generate QR code image
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Save QR code image to file
+    with open("qr.png", "wb") as f:
+        img.save(f)
+
+    # Send QR code image in current channel
+    await ctx.send("Aquí está tu código QR:", file=discord.File("qr.png"))
 
 # Run bot
-token = os.environ['TOKEN']
+token = os.environ["TOKEN"]
 bot.run(token)

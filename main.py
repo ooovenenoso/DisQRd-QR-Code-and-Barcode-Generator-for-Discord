dfd690c4aaf1b1ca78e51d510f20622fff438f2b
ooopoison <120500656+ooopoison@users.noreply.github.com>
2023-01-07 11:34:38 -0400

    Expanded functionality to include barcode
    "Added support for generating EAN13 and Code 39 barcodes"
    "Changed command prefix from '/' to '!'"
    "Updated docstrings with usage information for each command"
    "Imported necessary modules for generating EAN13 and Code 39 barcodes"

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 import discord
 from discord.ext import commands
 import qrcode
+import barcode
+from barcode.writer import ImageWriter
 
 # Set up Discord intents
 intents = discord.Intents.default()
@@ -14,14 +16,14 @@ intents.typing = True
 intents.message_content = True  # Add this line to enable the message contents intent
 
 # Create Discord bot
-bot = commands.Bot(command_prefix="/", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # QR code generation command
 @bot.command()
 async def qr(ctx, url: str):
-    """Generates a QR code for the given URL and sends it in the current channel.
+    """Generates a QR code for the given URL and sends it in the current channel as a PNG file.
     
-    Usage: /qr <url>
+    Usage: !qr <url>
     """
 
     # Create QR code object
@@ -41,8 +43,88 @@ async def qr(ctx, url: str):
     with open("qr.png", "wb") as f:
         img.save(f)
 
-    # Send QR code image in current channel
-    await ctx.send("Aquí está tu código QR:", file=discord.File("qr.png"))
+    # Send QR code PNG in current channel
+    await ctx.send("Aquí está tu código QR en formato PNG:", file=discord.File("qr.png"))
+
+# EAN13 barcode generation command
+@bot.command()
+async def ean13(ctx, data: str):
+    """Generates an EAN13 barcode for the given data and sends it in the current channel as a PNG file.
+    
+    Usage: !ean13 <data>
+    """
+
+    # Create EAN13 barcode object
+    ean13 = barcode.get_barcode_class('ean13')(data, writer=ImageWriter())
+
+    # Generate EAN13 barcode image
+    ean13.save('ean13')
+
+    # Send EAN13 barcode PNG in current channel
+    await ctx.send("Aquí está tu código de barras EAN13 en formato PNG:", file=discord.File("ean13.png"))
+
+# Code 39 barcode generation command
+@bot.command()
+async def code39(ctx, data: str):
+    """Generates a Code 39 barcode for the given data and sends it in the current channel as a PNG file.
+    
+    Usage: !code39 <data>
+    """
+
+    # Create Code 39 barcode object
+    code39 = barcode.get_barcode_class('code39')(data, writer=ImageWriter())
+
+    # Generate Code 39 barcode image
+    code39.save('code39')
+
+    # Send Code 39 barcode PNG in current channel
+    await ctx.send("Aquí está tu código de barras Code 39 en formato PNG:", file=discord.File("code39.png"))
+
+# Code 128 barcode generation command
+@bot.command()
+async def code128(ctx, data: str):
+    """Generates a Code 128 barcode for the given data and sends it in the current channel as a PNG file.
+    
+    Usage: !code128 <data>
+    """
+
+    # Create Code 128 barcode object
+    code128 = barcode.get_barcode_class('code128')(data, writer=ImageWriter())
+
+    # Generate Code 128 barcode image
+    code128.save('code128')
+
+    # Send Code 128 barcode PNG in current channel
+    await ctx.send("Aquí está tu código de barras Code 128 en formato PNG:", file=discord.File("code128.png"))
+
+# Help command
+@bot.command()
+async def hello(ctx):
+    """Shows a list of available commands and their usage.
+    
+    Usage: !hello
+    """
+
+    commands_list = [
+        ("!qr <url>", "Generates a QR code for the given URL."),
+        ("!ean13 <data>", "Generates an EAN13 barcode for the given data."),
+        ("!code39 <data>", "Generates a Code 39 barcode for the given data."),
+        ("!code128 <data>", "Generates a Code 128 barcode for the given data."),
+        ("!hello", "Shows a list of available commands and their usage."),
+    ]
+
+    commands_text = "\n".join([f"{c[0]}: {c[1]}" for c in commands_list])
+    await ctx.send(f"DisQRd: QR Code Generator\n\nAvailable commands:\n{commands_text}")
+
+# Info command
+@bot.command()
+async def info(ctx):
+    """Shows information about the bot.
+    
+    Usage: !info
+    """
+
+    await ctx.send("DisQRd: QR Code Generator\nAuthor: ooopoison\n\nhttps://github.com/ooopoison/DisQRd")
 
 # Run bot
 token = os.environ["TOKEN"]
